7e71f38a2a85d8c0bbf6a05439c5133fd3c7c787
Rucha <rucha.makwana1018@gmail.com>
2025-05-13 12:15:54 +0530

    add my portfolio in my branch

---
+++ MyPortfolio/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿
+    //// Toggle About Me Section
+    //function toggleAboutMe() {
+    //    const aboutSection = document.getElementById("aboutMeSection");
+    //aboutSection.classList.toggle("hidden");
+    //}
+
+    //// Rotate Role Text
+    //const roles = ["Developer", "Web Designer", "Freelancer"];
+    //let roleIndex = 0;
+    //setInterval(() => {
+    //    roleIndex = (roleIndex + 1) % roles.length;
+    //document.getElementById("rotating-role").textContent = roles[roleIndex];
+    //}, 2000);
+
